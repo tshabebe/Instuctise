f7b1c9b7da13def5fea320652f26a4ba192a4886
teshe <teshomeabebe114@gmail.com>
2024-12-09 20:52:58 +0000

    feat: theme provider

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import * as React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/primitives/button';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <ToggleTheme />
+    </div>
+  );
+}
+
+import { HTMLAttributes } from 'react';
+
+export function ToggleTheme(props: HTMLAttributes<HTMLButtonElement>) {
+  const { theme, setTheme } = useTheme();
+  return (
+    <>
+      <Button
+        size={'icon'}
+        variant={'ghost'}
+        onClick={
+          theme === 'dark' ? () => setTheme('light') : () => setTheme('dark')
+        }
+        {...props}
+      >
+        {theme === 'dark' ? <Moon /> : <Sun />}
+      </Button>
+    </>
+  );
 }
