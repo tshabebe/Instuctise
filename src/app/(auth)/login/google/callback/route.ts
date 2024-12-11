cb8e2f8dd79268af0030294ea35a65efc0f48512
teshe <teshomeabebe114@gmail.com>
2024-12-11 16:13:47 +0000

    fix(auth): remove the codeman comment

--- src/app/(auth)/login/google/callback/route.ts
+++ src/app/(auth)/login/google/callback/route.ts
@@ -1,6 +1,3 @@
 import { validateGoogleCallback } from '@/auth/google.provider';
 
-export {
-  /* @next-codemod-error `validateGoogleCallback` export is re-exported. Check if this component uses `params` or `searchParams`*/
-  validateGoogleCallback as GET,
-};
+export { validateGoogleCallback as GET };
--- src/app/(auth)/login/google/route.ts
+++ src/app/(auth)/login/google/route.ts
@@ -1,6 +1,3 @@
 import { createGoogleAuthorizationURL } from '@/auth/google.provider';
 
-export {
-  /* @next-codemod-error `createGoogleAuthorizationURL` export is re-exported. Check if this component uses `params` or `searchParams`*/
-  createGoogleAuthorizationURL as GET,
-};
+export { createGoogleAuthorizationURL as GET };
